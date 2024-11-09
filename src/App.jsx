af0d4757533dc6e7a23769f125077f4b7ab2241b
saurav29kumar <sauravkushwaha2001@gmail.com>
2024-11-09 18:18:17 +0530

    first time posting

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Card from "./card";
+function App(){
+  const raw = [{name:"saurav",profession:"coder", image:"https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fHww" , friends: false},
+    {name:"Ashirbad",profession:"programer", image:"https://images.unsplash.com/photo-1480455624313-e29b44bbfde1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWFufGVufDB8fDB8fHww", friends: false},
+    {name:"Hementh",profession:"coder", image:"https://images.unsplash.com/photo-1557862921-37829c790f19?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1hbnxlbnwwfHwwfHx8MA%3D%3D", friends: false},
+    {name:"Prafull",profession:"coder", image:"https://images.unsplash.com/photo-1568602471122-7832951cc4c5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1hbnxlbnwwfHwwfHx8MA%3D%3D", friends: false},
+    {name:"Prince",profession:"coder", image:"https://images.unsplash.com/photo-1495603889488-42d1d66e5523?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG1hbnxlbnwwfHwwfHx8MA%3D%3D", friends: false}
+    
+  ]
+  const [data,setData] = useState(raw);
+  const handleClick = (changingIndex)=>{
+    setData((prev)=>{
+      return prev.map((item,index)=>{
+        if(index === changingIndex ) return {...item, friends: !item.friends}
+        return item;
+      })
+    })
+  }
+  return(
+    <div className="bg-zinc-800 h-screen w-screen flex items-center justify-center space-x-4">
+      {data.map((item,index)=>(
+        <Card key={index} index={index} handleClick={handleClick} values={item}/>
+      ))}
+      
+    </div>
+  )
+}
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+function Card({values , index, handleClick}){
+  const {name,profession,image,friends} = values;
+    return(
+      <div className="bg-white w-60 rounded-md overflow-hidden">
+        <div className="h-40 bg-blue-300">
+          <img className="h-full object-fill" src={image} alt="" />
+        </div>
+        <div className="p-1">
+          <h3 className="font-bold">{name}</h3>
+          <h6 className="text-sm">{profession}</h6>
+          <button onClick={()=>handleClick(index)} className={`px-3 py-1 text-white items-center rounded-full ${friends === true ? "bg-green-500":"bg-red-400"} mt-2`}>{friends === true ? "Friends" : "Add friend"}</button>
+        </div>
+      </div>
+    )
+}
+export default Card;
